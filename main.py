fde9a214f07f90d53db36e9a1d7969cb10272de4
Cynchronized <austin.thieu123@gmail.com>
2025-02-09 14:01:05 -0700

    Create sprite for player as triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from player import Player 
 
 
 def main():
@@ -7,6 +8,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         # Exit loop when user closes the window
@@ -14,12 +18,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # create black background
         screen.fill("black")
+
+        # Draw the player
+        player.draw(screen)
+
         # Refresh screen
         pygame.display.flip()
 
         # limit the frame rate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 
 
